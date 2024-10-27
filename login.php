0ed76ad12224486afe6aeab441d18bf23b27ed31
Brayan <bchacon959@gmail.com>
2024-10-27 16:12:12 -0600

    Brayan
    .

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+$cedula = $_POST['cedula'];
+$password = $_POST['password'];
+
+if ($cedula == '001010111' && $password == '0000') {
+    header("Location: admin.html");
+    exit();
+} elseif (
+    ($cedula == '101010111' && $password == '1111') ||
+    ($cedula == '201010111' && $password == '2222')
+) {
+    header("Location: empleados.html");
+    exit();
+} elseif (
+    ($cedula == '301010111' && $password == '3333') ||
+    ($cedula == '401010111' && $password == '4444')
+) {
+    header("Location: veterinarios.html");
+    exit();
+} else {
+    echo "<script>alert('Cédula o contraseña incorrecta'); window.location.href='index.html';</script>";
+}
+?>

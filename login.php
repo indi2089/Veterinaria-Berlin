d2074f339f55bea965e7bbcda6200aca6d1436ce
Brayan <bchacon959@gmail.com>
2024-11-03 18:45:56 -0600

    Brayan
    Cambios en empleados, veterinarios

--- login.php
+++ login.php
@@ -2,15 +2,18 @@
 $cedula = $_POST['cedula'];
 $password = $_POST['password'];
 
+//Perfil admin
 if ($cedula == '001010111' && $password == '0000') {
     header("Location: admin.html");
     exit();
+    //Perfil Empleados
 } elseif (
     ($cedula == '101010111' && $password == '1111') ||
     ($cedula == '201010111' && $password == '2222')
 ) {
     header("Location: empleados.html");
     exit();
+    //Perfil veterinarios
 } elseif (
     ($cedula == '301010111' && $password == '3333') ||
     ($cedula == '401010111' && $password == '4444')
